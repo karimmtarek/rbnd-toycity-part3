c83014016ea10c43f37795a956055fcc91b78d5c
Karim Tarek <karimmtarek@gmail.com>
2016-01-13 14:47:28 -0500

    Add find and all methods to transactions

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,10 +3,20 @@ class Transaction
   @@transaction = []
 
   def initialize(customer, product)
+    raise OutOfStockError.new "'#{product.title}' is out of stock." unless product.in_stock?
+
     @customer = customer
     @product = product
     @id = @@transaction.length + 1
     @product.update_product_inventory
     @@transaction << self
+  end
+
+  def self.find(id)
+    all.find { |transaction| transaction.id == id }
+  end
+
+  def self.all
+    @@transaction
   end
 end
